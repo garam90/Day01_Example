6d4b23c373fa2891b27ee55a279f5d76295aabc5
나가람/ICTO개발팀 <garam901127@gmail.com>
2015-04-29 16:21:39 +0900

    day01_example

---
+++ day01_point/PointTest.java
@@ -0,0 +1,29 @@
+package day01_point;
+
+public class PointTest {
+
+	public static void main(String[] args) {
+		
+	/*	Point shape = new Point();
+		shape.setX(2);
+		shape.setY(5);
+		shape.show(true);
+		shape.show();
+		
+		shape.setX(10);
+		shape.setY(23);
+		shape.show();
+		shape.show(false);
+		
+		Point shape2 = new Point(50, 50);
+		shape2.show(true);
+		shape2.show(false);*/
+		
+		ColorPoint point4 = new ColorPoint(10,50,"red");
+		/*point4.setColor("red");
+		point4.setX(10);
+		point4.setY(50);*/
+		point4.show();
+		
+	}
+}
